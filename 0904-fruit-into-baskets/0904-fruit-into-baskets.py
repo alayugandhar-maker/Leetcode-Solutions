407e3b67ad320793c3f0276348f4754e79a2b296
Yugandhar <alayugandhar@gmail.com>
2026-09-13 18:54:55 +0530

    Time: 161 ms (86.95%), Space: 25.7 MB (84.48%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -7,7 +7,7 @@ class Solution:
 
         while r<n:
             d[fruits[r]]=d.get(fruits[r],0)+1
-            while len(d)>2:
+            if len(d)>2:
                 d[fruits[l]]-=1
                 if d[fruits[l]]==0:
                     del d[fruits[l]]
